cfe7882128f8f073ec97ebd0945889104528b429
Peter Wintsch <pwintsch@aragorn.home>
2016-08-10 10:38:39 +0200

    econd commit using unittests

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,43 @@
 from selenium import webdriver
+import unittest
 
-browser=webdriver.Firefox()
-browser.get('http://localhost:8000')
+class NewVisitorTest(unittest.TestCase):   #
 
-assert 'Django' in browser.title
+    def setUp(self):     #
+        self.browser=webdriver.Firefox()
+        self.browser.implicitly_wait(3)
+    
+    def tearDown(self):   #
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self): #
+        
+
+#Edith has haerd of a cool new todo app she goes to check out its home page
+
+        self.browser.get('http://localhost:8000')
+
+#She notices the page title  and header mention To-do Lists
+
+        self.assertIn('To-Do', self.browser.title) #
+        self.fail('Finish the test') #
+
+#she is invited to enter a todo item straight away
+
+# She types buy peacock feathers into a text box
+
+# When she hits enter the page updates displaying "by peacock feathers " as an item
+
+# There is a text box inviting her to add another item. She enters "use peacock feather to tie a fly"
+
+# The page updates again and shows both items on a list
+
+# Edith wonders whether the site will remember her lists but sees it has generated a unique URL for her
+# There is text describing that
+
+# Edith Visits the URL the list is still there
+
+# Edith goes back to sleep
+
+if __name__=='__main__': #
+    unittest.main(warnings='ignore') #
